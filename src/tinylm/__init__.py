10efd4d04a7a515ee6397f75262c21d8fd2e6560
Kaushik Sinha <inftloop@gmail.com>
2026-02-12 09:03:05 -0500

    Add Markov class

--- src/tinylm/__init__.py
+++ src/tinylm/__init__.py
@@ -1,2 +1,33 @@
+import random
+
+class Markov:
+    def __init__(self, txt: str, size: int = 1) -> None:
+        self.tables = [get_table(txt, size = i + 1) for i in range(size)]
+
+    def predict(self, txt: str) -> str:
+        table = self.tables[len(txt) - 1]
+        next_counts = table.get(txt, {})
+        if not next_counts:
+            raise KeyError(f"{txt} not found")
+        options: list[str] = []
+        for next_char, count in next_counts.items():
+            options.extend([next_char] * count)
+        return random.choice(options)
+
+    def get_table(txt: str, size: int = 1) -> dict[str, dict[str, int]]:
+        results: dict[str, dict[str, int]] = {}
+        for i in range(len(txt)):
+            chars = txt[i + i + size]
+            try:
+                out = txt[i + size]
+            except IndexError:
+                break
+
+            char_dict = results.get(chars, {})
+            char_dict.setdefault(out, 0)
+            char_dict[out] += 1
+            results[char] = char_dict
+        return results
+
 def main() -> None:
     print("Hello from tinylm!")
